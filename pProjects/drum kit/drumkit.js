d139716fb674191d830ed0f9233dc7238f9a7b27
Piyush-kr-Sinha <piyushkumarofficial.2503@gmail.com>
2023-05-20 12:42:13 +0530

    First Push Folder on git

---
+++ pProjects/drum kit/drumkit.js
@@ -0,0 +1,19 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+function removeTransition(e) {
+    // const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+    console.log(this);
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+
